22c499775a29993f8ccf1ebb34f0bd6585a70095
tiagoRaulino <tiagoraulino542@gmail.com>
2024-08-29 11:53:14 -0300

    pesquisa funcionando, component Card e roomData extraidos

--- app/components/MyCard.tsx
+++ app/components/MyCard.tsx
@@ -1,28 +1,36 @@
-import { Box, Card, Flex, Text, Badge, Button } from '@radix-ui/themes';
+import { Card, Flex, Text, Badge, Box, Button } from "@radix-ui/themes";
 
-function MyCard() {
-    return (
-        <div>
-            <Box maxWidth="480px">
-                <Card>
-                    <Flex gap="3" align="center">
-                        <Box>
-                            <Text as="div" size="3" weight="bold">
-                                Sala 1
-                            </Text>
-                            <Text as="div" size="2" color="gray">
-                                Posse da chave: Marina Santos
-                            </Text>
-                        </Box>
-                        <Flex gap="2" ml={"auto"}>
-                            <Badge color="green">Disponivel</Badge>
-                            <Button ml={"3"}>Repassar</Button>
-                        </Flex>
-                    </Flex>
-                </Card>
-            </Box>
-        </div>
-    )
+interface MyCardProps {
+    room: string;
+    keyowner: string;
+    available: boolean;
 }
 
-export default MyCard
+const MyCard: React.FC<MyCardProps> = ({ room, keyowner, available }) => {
+    return (
+        <Box maxWidth="480px">
+            <Card>
+                <Flex gap="3" align="center">
+                    <Box>
+                        <Text as="div" size="3" weight="bold">
+                            {room}
+                        </Text>
+                        <Text as="div" size="2" color="gray">
+                            {keyowner}
+                        </Text>
+                    </Box>
+                    <Flex gap="2" ml={"auto"}>
+                        {available ? (
+                            <Badge color="green">Disponivel</Badge>
+                        ) : (
+                            <Badge color="red">Indisponivel</Badge>
+                        )}
+                        {keyowner === "you" && <Button ml={"3"}>Repassar</Button>}
+                    </Flex>
+                </Flex>
+            </Card>
+        </Box>
+    );
+};
+
+export default MyCard;
--- app/components/MyList.tsx
+++ app/components/MyList.tsx
@@ -1,32 +1,56 @@
-import React from 'react'
-import MyCard from './MyCard'
-import SearchBar from './SearchBar'
-import { Box } from '@radix-ui/themes'
+'use client'
 
+import { TextField, Container } from "@radix-ui/themes";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useState, useEffect } from 'react';
+import MyCard from './MyCard';
 
 interface RoomData {
-    room: string[];
+    room: string;
     keyowner: string;
     available: boolean;
     open: boolean;
 }
 
-
 function MyList() {
-    const roomData: RoomData = require("./RoomData.json");
-    console.log(roomData.room);
+    const [search, setSearch] = useState<string>('');
+    const [roomData, setRoomData] = useState<RoomData[]>([]);
+
+    useEffect(() => {
+        fetch('../roomData.json')
+            .then(response => response.json())
+            .then(data => setRoomData(data))
+            .catch(error => console.error('Error fetching room data:', error));
+    }, []);
+
+    const filteredRooms = roomData.filter((data) =>
+        data.room.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <div>
-            <Box maxWidth="480px">
-                <SearchBar data={roomData.room} placeholder={"Busque uma sala pelo nome"} />
-            </Box>
-            <MyCard />
-            <MyCard />
-            <MyCard />
-            <MyCard />
-            <MyCard />
+            <Container maxWidth={"480px"}>
+                <form className="d-flex" role="search">
+                    <TextField.Root
+                        type="text"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        placeholder="Search">
+                        <TextField.Slot>
+                            <MagnifyingGlassIcon height="16" width="16" />
+                        </TextField.Slot>
+                    </TextField.Root>
+                </form>
+                {filteredRooms.map((data, index) => (
+                    <MyCard
+                        room={data.room}
+                        keyowner={data.keyowner}
+                        available={data.available}
+                    />
+                ))}
+            </Container>
         </div>
-    )
+    );
 }
 
-export default MyList
+export default MyList;
